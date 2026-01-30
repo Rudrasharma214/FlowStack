c7f33cc08a59fa77813df294908486872aeea69f
Rudrasharma214 <rudrashma.810@gmail.com>
2026-01-30 12:45:23 +0530

    feat: enhance root route response and update database connection configuration

--- Backend/src/app.mjs
+++ Backend/src/app.mjs
@@ -1,6 +1,8 @@
 import express from 'express';
 import { errorHandler } from './core/middlewares/error.middleware.js';
 import authRouter from './core/modules/auth/routes/auth.route.js';
+import { sendResponse } from './core/utils/response.js';
+import { STATUS } from './core/constants/statusCodes.js';
 
 const app = express();
 
@@ -9,7 +11,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  sendResponse(res, STATUS.OK, 'Server is healthy', {
+    version: 'TMS-1.0',
+    ip: req.ip,
+    healthy: true,
+    requestedAt: new Date().toLocaleString("en-IN", {
+      timeZone: "Asia/Kolkata",
+      hour12: false
+    }),
+  });
 });
 
 app.use('/api/auth', authRouter);
